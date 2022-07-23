454ad704f0434ea0cf71ea4617a2878e4c234325
gzgoel <gxgoel@gmail.com>
2022-07-23 13:31:33 +0530

    Merge remote-tracking branch 'origin/master'

--- Roboshop-shellscript/Components/mongodb.sh
+++ Roboshop-shellscript/Components/mongodb.sh
@@ -1,1 +1,8 @@
 echo installing mongodb  component
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+systemctl restart mongod
